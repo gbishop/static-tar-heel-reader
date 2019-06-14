07464b5fbfb38f011f01e111990edbb4b51a7a6a
Gary Bishop <gary.bishop.unc@gmail.com>
2019-06-14 09:15:00 -0400

    working simple search first page

--- find.ts
+++ find.ts
@@ -14,10 +14,8 @@
 import {stem} from 'stemr';
 
 function getQueryTerms(): string[] {
-  const searchBox = document.getElementById('search') as HTMLInputElement;
-  console.log(searchBox);
+  const searchBox = document.querySelector('#search') as HTMLInputElement;
   const query = searchBox.value;
-  console.log('q', query);
   const pattern = /[a-z]{3,}/gi;
   const terms = query.match(pattern).map(stem);
   return terms;
@@ -85,21 +83,19 @@ async function getBookCover(bid: string): Promise<HTMLElement | null> {
   const item = doc.getElementById(bid);
   if (item) {
     // fix the image URL
-    const imgs = item.getElementsByTagName('img');
-    const img: HTMLImageElement = imgs[0];
+    const img: HTMLImageElement = item.querySelector('img');
     img.setAttribute('src', prefix + img.getAttribute('src'));
     // fix the link URL
-    const links = item.getElementsByTagName('a');
-    const link: HTMLAnchorElement = links[0];
+    const link: HTMLAnchorElement = item.querySelector('a');
     link.setAttribute('href', prefix + link.getAttribute('href'));
   }
   return item;
 }
 
 function addBookToPage(book: HTMLElement) {
-  const list = document.getElementsByTagName('ul');
+  const list = document.querySelector('ul');
   if (list) {
-    list[0].appendChild(book);
+    list.appendChild(book);
   }
 }
 
@@ -113,9 +109,8 @@ async function find() {
     }
   }
   let i = 0;
-  const list = document.getElementsByTagName('ul');
-  if (list) {
-    const node = list[0];
+  const node = document.querySelector('ul');
+  if (node) {
     let last;
     while ((last = node.lastChild)) node.removeChild(last);
   }
@@ -132,15 +127,48 @@ async function find() {
   }
 }
 
+interface SearchSettings {
+  search: string,
+  reviewed: boolean,
+  category: string,
+  audience: string,
+  page: number
+}
+
+function getSearchSettings(): SearchSettings {
+  const form: HTMLFormElement = document.querySelector('form');
+  let result: SearchSettings = {
+    search: form.search.value,
+    reviewed: form.reviewed.value == 'R',
+    category: form.category.value,
+    audience: form.audience.value,
+    page: parseInt(form.page.value),
+  }
+  return result;
+}
+
+function setSearchSettings(v: SearchSettings): void {
+  const form: HTMLFormElement = document.querySelector('form');
+  form.search.value = v.search;
+  form.reviewed.value = v.reviewed ? 'R' : 'U';
+  form.category.value = v.category;
+  form.audience.value = v.audience;
+  form.page.value = v.page;
+}
+
 function init() {
-  const form = document.getElementsByTagName('form');
-  if (form.length) {
-    form[0].addEventListener('submit', e => {
+  const form = document.querySelector('form');
+  if (form) {
+    form.addEventListener('submit', e => {
       e.preventDefault();
       find();
     });
   }
   find();
+  let s = getSearchSettings();
+  s.page += 1;
+  setSearchSettings(s);
+  console.log(getSearchSettings());
 }
 
 window.addEventListener('load', init);
