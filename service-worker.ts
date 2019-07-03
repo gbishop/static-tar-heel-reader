c6d89af7c057e4416d32e388e55fc1885dcdb060
dreamist <dreamist@live.unc.edu>
2019-07-03 13:21:30 -0400

    Implemented and routed getAllAvailableIDs.

--- service-worker.ts
+++ service-worker.ts
@@ -1,6 +1,4 @@
-import { QueryObject, QueryObjectI, QueryType } from './QueryObject';
-
-console.log("Hello from service worker!");
+import { RangeSet } from './BookSet';
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
@@ -23,7 +21,7 @@ workbox.routing.registerRoute(
 
 
 workbox.routing.registerRoute(
-    /.html|.css|.js/,
+    /.html|.css|.js|.json/,
     new workbox.strategies.CacheFirst({
         cacheName: "html-cache",
         plugins: [
@@ -34,7 +32,75 @@ workbox.routing.registerRoute(
     })
 );
 
-let x = this;
+workbox.routing.registerRoute(
+    /\/content\/index\/AllAvailable$/,
+    async () => {
+        let ids = getAllAvailableIDs();
+        return new Response(await ids);
+    }
+);
+
+workbox.routing.registerRoute(
+    /\/content\/index/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "index-cache",
+        plugins: [
+            new workbox.expiration.Plugin({
+                maxAgeSeconds: 30*24*60*60
+            })
+        ],
+    })
+);
+
+interface Config {
+  base: number; // base of the encoding used for ids
+  digits: number; // number of digits in each id
+  lastReviewed: string; // id of last reviewed book
+  first: string; // id of first book
+  last: string; // id of last book
+}
+
+// Fetches the available IDs. For offline case, can just generate a RangeSet.
+async function getAllAvailableIDs(): Promise<string>{
+    if (navigator.onLine){
+        let config: Config = await (await fetch("content/config.json")).json();
+        let range = new RangeSet(config.first, config.last, config.digits, config.base);
+        let ids = "";
+        while (true){
+            let curr = range.next();
+            if(!curr) { break; }
+            ids += curr;
+        }
+        return ids;
+    }
+
+    // Offline case.
+    let ids: string = "";
+    return new Promise((resolve, reject) => {
+        caches.open('html-cache').then((cache) => {
+            cache.keys().then((keys) => {
+                if (keys.length == 0){
+                    resolve("");
+                }
+
+                keys.forEach((request, index, array) => {
+                    let url = request.url;
+                    if (!url.includes('content')){
+                        return;
+                    }
+                    let tokens = url.substring(url.search('content') + 'content'.length).split('/');
+                    if (!tokens[tokens.length-1].match(/\d.html/)){
+                        return;
+                    }
+                    let id = tokens.join('');
+                    id = id.substring(0, id.length-5);
+                    ids += id;
+                });
+                resolve(ids);
+            })
+        });
+    });
+}
 
 /**
  * Example of communicating with the Service Worker like any other worker: messages.
@@ -42,13 +108,14 @@ let x = this;
  * 
  * Alternative would be to communicate by intercepting fetches.
  */
+/*
 self.addEventListener('message', function(event){
     // There MUST be a better type guard. Expecting QueryObject.
     if ('query_param' in event.data &&
         'query_type' in event.data){
         if (event.data.query_type === QueryType.GET_CACHEDIDS){
 
-            let ids: string[] = [];
+            let ids: string = "";
 
             caches.open('html-cache').then((cache) => {
                 cache.keys().then((keys) => {
@@ -63,7 +130,7 @@ self.addEventListener('message', function(event){
                         }
                         let id = tokens.join('');
                         id = id.substring(0, id.length-5);
-                        ids.push(id);
+                        ids += id;
                     });
                     event.ports[0].postMessage(ids);
                 });
@@ -74,3 +141,4 @@ self.addEventListener('message', function(event){
         console.warn("Something not quite working. QueryObject not received.");
     }
 });
+*/
