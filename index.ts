e505b9f3174fa6b2ef597583daa1cfedcc7569dd
Gary Bishop <gary.bishop.unc@gmail.com>
2019-07-05 09:39:30 -0400

    service worker changes.

--- index.ts
+++ index.ts
@@ -1,55 +1,54 @@
 // Registers service worker and begins work on detecting online status, changing visual state to reflect that.
 
-if ('serviceWorker' in navigator){
-    window.addEventListener('load', ()=> {
-        navigator.serviceWorker.register('../../worker.js')
-            .then(function(){
-                console.log("Service worker registered!");
-            });
-    })
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("worker.js").then(function() {
+      console.log("Service worker registered!");
+    });
+  });
 }
-
 
 /**
  * Uses a heuristic in fetching to mark books that have been cached.
  * Moving now to directly querying the cache in the service worker.
  * TODO: Use AllAvailable instead of heuristic fetches.
  */
-async function markAvailableBooks(){
-    let book_entries: HTMLCollection = document.getElementsByClassName("R");
-    for(let i = 0; i < book_entries.length; i++){
-        let entry = book_entries[i];
-        if (entry instanceof HTMLElement){
-            let href = entry.getAttribute("href");
-            if (href !== undefined){
-                let f = fetch(href)
-                    .then(() => {
-                        (entry as HTMLElement).style.backgroundColor = "green";
-                        console.log(`Changed color to indicate ${href} has already been cached.`);
-                    })
-                    .catch(() => {
-                        console.log(`Failed to fetch ${href} from cache.`);
-                    });
-            }
-        }
-        
+async function markAvailableBooks() {
+  let book_entries: HTMLCollection = document.getElementsByClassName("R");
+  for (let i = 0; i < book_entries.length; i++) {
+    let entry = book_entries[i];
+    if (entry instanceof HTMLElement) {
+      let href = entry.getAttribute("href");
+      if (href !== undefined) {
+        let f = fetch(href)
+          .then(() => {
+            (entry as HTMLElement).style.backgroundColor = "green";
+            console.log(
+              `Changed color to indicate ${href} has already been cached.`
+            );
+          })
+          .catch(() => {
+            console.log(`Failed to fetch ${href} from cache.`);
+          });
+      }
     }
+  }
 }
 
-function onlineHook (){
-    console.info("We're online! Here can load new contents.");
+function onlineHook() {
+  console.info("We're online! Here can load new contents.");
 }
 
-function offlineHook (){
-    markAvailableBooks();
+function offlineHook() {
+  markAvailableBooks();
 }
 
-window.addEventListener('load', ()=> {
-    if (!navigator.onLine){
-        alert("Offline mode! Highlighting cached entries.");
-        offlineHook();
-    } else {
-        console.log("Already online.");
-    }
-    window.addEventListener('online', onlineHook);
+window.addEventListener("load", () => {
+  if (!navigator.onLine) {
+    alert("Offline mode! Highlighting cached entries.");
+    offlineHook();
+  } else {
+    console.log("Already online.");
+  }
+  window.addEventListener("online", onlineHook);
 });
--- service-worker.ts
+++ service-worker.ts
@@ -1,55 +1,53 @@
-import { RangeSet } from './BookSet';
+import { RangeSet } from "./BookSet";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+);
 
 declare var workbox: any;
 
-workbox.loadModule('workbox-strategies');
-workbox.loadModule('workbox-precaching');
+workbox.loadModule("workbox-strategies");
+workbox.loadModule("workbox-precaching");
 
 workbox.routing.registerRoute(
-    /.jpg/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "img-cache",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 30*24*60*60
-            })
-        ],
-    })
-);
-
-
-workbox.routing.registerRoute(
-    /.html|.css|.js|.json/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "html-cache",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 30*24*60*60
-            })
-        ],
-    })
+  /.jpg/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "img-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  })
 );
 
 workbox.routing.registerRoute(
-    /\/content\/index\/AllAvailable$/,
-    async () => {
-        let ids = getAllAvailableIDs();
-        return new Response(await ids);
-    }
+  /.html|.css|.js|.json/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "html-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  })
 );
 
+workbox.routing.registerRoute(/.\/content\/index\/AllAvailable$/, async () => {
+  let ids = getAllAvailableIDs();
+  return new Response(await ids);
+});
+
 workbox.routing.registerRoute(
-    /\/content\/index/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "index-cache",
-        plugins: [
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 30*24*60*60
-            })
-        ],
-    })
+  /.\/content\/index/,
+  new workbox.strategies.CacheFirst({
+    cacheName: "index-cache",
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60
+      })
+    ]
+  })
 );
 
 interface Config {
@@ -61,43 +59,52 @@ interface Config {
 }
 
 // Fetches the available IDs. For offline case, can just generate a RangeSet.
-async function getAllAvailableIDs(): Promise<string>{
-    if (navigator.onLine){
-        let config: Config = await (await fetch("content/config.json")).json();
-        let range = new RangeSet(config.first, config.last, config.digits, config.base);
-        let ids = "";
-        while (true){
-            let curr = range.next();
-            if(!curr) { break; }
-            ids += curr;
-        }
-        return ids;
+async function getAllAvailableIDs(): Promise<string> {
+  if (navigator.onLine) {
+    let config: Config = await (await fetch("content/config.json")).json();
+    let range = new RangeSet(
+      config.first,
+      config.last,
+      config.digits,
+      config.base
+    );
+    let ids = "";
+    while (true) {
+      let curr = range.next();
+      if (!curr) {
+        break;
+      }
+      ids += curr;
     }
+    return ids;
+  }
 
-    // Offline case.
-    let ids: string = "";
-    return new Promise((resolve, reject) => {
-        caches.open('html-cache').then((cache) => {
-            cache.keys().then((keys) => {
-                if (keys.length == 0){
-                    resolve("");
-                }
+  // Offline case.
+  let ids: string = "";
+  return new Promise((resolve, reject) => {
+    caches.open("html-cache").then(cache => {
+      cache.keys().then(keys => {
+        if (keys.length == 0) {
+          resolve("");
+        }
 
-                keys.forEach((request, index, array) => {
-                    let url = request.url;
-                    if (!url.includes('content')){
-                        return;
-                    }
-                    let tokens = url.substring(url.search('content') + 'content'.length).split('/');
-                    if (!tokens[tokens.length-1].match(/\d.html/)){
-                        return;
-                    }
-                    let id = tokens.join('');
-                    id = id.substring(0, id.length-5);
-                    ids += id;
-                });
-                resolve(ids);
-            })
+        keys.forEach((request, index, array) => {
+          let url = request.url;
+          if (!url.includes("content")) {
+            return;
+          }
+          let tokens = url
+            .substring(url.search("content") + "content".length)
+            .split("/");
+          if (!tokens[tokens.length - 1].match(/\d.html/)) {
+            return;
+          }
+          let id = tokens.join("");
+          id = id.substring(0, id.length - 5);
+          ids += id;
         });
+        resolve(ids);
+      });
     });
+  });
 }
